3c3a16480812f655083b639d791794e1a50bdfc0
Tawanda <ejmunongo@gmail.com>
2022-07-24 06:58:38 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//local imports
+import Bar from './components/Bar'
+
+//style
+import './App.css'
+
+class App extends Component {
+  state = {
+    array: [],
+    arraySteps: [],
+    colorCodes: [],
+    colorSteps: [],
+    currentStep: 0,
+    count: 10,
+    delay: 100,
+    algorithm: '',
+    timeouts: [],
+  }
+
+  componentDidMount() {
+    this.genRandomArray()
+  }
+
+  genRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+
+  genRandomArray = () => {
+    const count = this.state.count
+    const temp = []
+
+    for (let i = 0; i < count; i++) {
+      temp.push(this.genRandomNumber(50, 200))
+    }
+
+    this.setState({
+      array: temp,
+      arraySteps: [temp],
+    })
+  }
+
+  render() {
+    let bars = this.state.array.map((value, index) => {
+      return (
+        <Bar
+          key={index}
+          index={index}
+          length={value}
+          color={this.state.colorCodes[index]}
+        />
+      )
+    })
+    return <div className="app">{bars}</div>
+  }
 }
 
-export default App;
+export default App
